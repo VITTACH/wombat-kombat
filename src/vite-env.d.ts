e658aa1483dafbf90280d773bc394a0ed3d0abc5
Vittach <VVZharikov@spb.beeline.ru>
2024-07-09 03:37:44 +0300

    try to fix build failure

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+interface Window {
+    Telegram: {
+        WebApp: {
+            initDataUnsafe: {
+                user?: {
+                    username?: string;
+                };
+            };
+            ready: () => void;
+            expand: () => void;
+        };
+    };
+}
