681ca5d6e37e359615b8347c0e453ba5bbc314ec
Vittach <VVZharikov@spb.beeline.ru>
2024-07-09 03:25:19 +0300

    add navigation component

---
+++ src/about/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const About: React.FC = () => {
+    return <Link to="/">
+        <div>Home Screen</div>
+    </Link>
+};
+
+export default About;
