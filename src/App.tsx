9f416de9facc6bcf3bf702050857f7e8ec6f10e5
Vittach <VVZharikov@spb.beeline.ru>
2024-07-12 01:29:34 +0300

    попытка добавить навигацию

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import About from './about/About';
+import { useNavigate, useLocation } from 'react-router-dom';
+import WebApp from '@twa-dev/sdk';
 
 const App: React.FC = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        WebApp.ready();
+
+        // Обработка нажатия кнопки "назад"
+        WebApp.BackButton.onClick(() => {
+        navigate(-1);
+        });
+
+        // Слушаем изменения в истории для управления кнопкой "назад"
+        if (location.pathname !== '/') {
+        WebApp.BackButton.show();
+        } else {
+        WebApp.BackButton.hide();
+        }
+    }, [location, navigate]);
+    
     return (
         <Router>
             <Routes>
